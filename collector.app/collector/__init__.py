fbddcaa724026a80249684163e7202103c9d4e3e
Egor Galkin <egor.galkin99@yandex.ru>
2018-10-08 12:18:39 +0300

    Added parser initialization

--- collector.app/collector/__init__.py
+++ collector.app/collector/__init__.py
@@ -1,6 +1,14 @@
-from collector.api_folder.collecotr_api import Collector, PlotBuilder
 from flask import Flask
-from flask_restful import Api
+from flask_restful import Api, reqparse
 
 app = Flask(__name__)
 api = Api(app, prefix='/api/v1')
+
+collector_args = ['url', 'num_of_responses', 'last_mod', 'status']
+plots_info = dict()
+collector_parser = reqparse.RequestParser()
+for arg in collector_args:
+    collector_parser.add_argument(arg)
+
+
+from collector import api_routes
